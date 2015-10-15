2c6a83e86e4a802d951bacb96f21a7e458279e2d
Jeffrey Schultz <schultzjl@verizon.net>
2015-10-14 20:47:32 -0400

    Added minimal logging to mn.sh to track files processed.

--- scripts/mn.sh
+++ scripts/mn.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 #
+exec 3>&2 # logging stream (file descriptor 3) defaults to STDERR
+verbosity=3 # default to show warnings
+silent_lvl=0
+crt_lvl=1
+err_lvl=2
+wrn_lvl=3
+dbg_lvl=4
+inf_lvl=5
+
 base=$(dirname "$0")
 if [ $base = '.' ]; then
 	base=$(pwd)
@@ -12,10 +21,25 @@ wget -o wget.log -r -l1 --no-parent -nd -nc -P ${data} --user=clarus --password=
 
 echo Processing wget log to retrieve list of files downloaded...
 for file in $(egrep  "surface.*txt.*saved" wget.log | cut -d ' ' -f 6 | sed 's/^.\(.*\).$/\1/'); do
-	echo Applying awk script to $file...
-	$base/fix-mn-csv-firstcolumn.awk $file > $file.tmp
-	mv $file.tmp $file
+	(
+		echo -n Applying awk script to $file...
+		$base/fix-mn-csv-firstcolumn.awk $file > $file.tmp
+		mv $file.tmp $file
+		echo Done.
+	) | tee --append mn.log 
 done
+echo Complete.
 
-echo Done.
-
+notify() { log $silent_lvl "NOTE: $1"; } # Always prints
+critical() { log $crt_lvl "CRITICAL: $2"; }
+error() { log $err_lvl "ERROR: $1"; }
+warn() { log $wrn_lvl "WARNING: $1"; }
+debug() { log $dbg_lvl "DEBUG: $1"; }
+inf() { log $inf_lvl "INFO: $1"; } # "info" is already a command
+log() {
+    if [ $verbosity -ge $1 ]; then
+        datestring=`date +'%Y-%m-%d %H:%M:%S'`
+        # Expand escaped characters, wrap at 70 chars, indent wrapped lines
+        echo -e "$datestring $2" | fold -w70 -s | sed '2~1s/^/  /' >&3
+    fi
+}
